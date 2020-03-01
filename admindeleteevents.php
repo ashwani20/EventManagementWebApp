82c2174c6e174e9a2bcc2851dbf65162c79021e0
ashwani20 <kratikagupta808@gmail.com>
2020-03-01 02:27:26 -0500

    Made changes to incorporate deletion in event management. Also, created a browse page for sessions

--- admindeleteevents.php
+++ admindeleteevents.php
@@ -8,6 +8,23 @@
     include_once 'classes/PDO.DB.class.php';
     if(isset($_GET['delId']) and $_GET['delId'] !=""){
         $dbObj = new DB();
+
+        // Removing records from sessions table having event ID similiar to passed event ID
+        $count=$dbObj->getDBH()->prepare("DELETE FROM session WHERE event =:id");
+        $count->bindParam(":id", $_GET['delId'], PDO::PARAM_INT);
+        $count->execute();
+
+        // Removing records from manager_events table having event ID similiar to passed event ID
+        $count=$dbObj->getDBH()->prepare("DELETE FROM manager_event WHERE idevent =:id");
+        $count->bindParam(":id", $_GET['delId'], PDO::PARAM_INT);
+        $count->execute();
+
+        // Removing records from attendee_events table having event ID similiar to passed event ID
+        $count=$dbObj->getDBH()->prepare("DELETE FROM attendee_event WHERE idevent =:id");
+        $count->bindParam(":id", $_GET['delId'], PDO::PARAM_INT);
+        $count->execute();
+
+        // Finally removing record from event table having event ID similiar to passed event ID
         $count=$dbObj->getDBH()->prepare("DELETE FROM event WHERE idevent =:id");
         $count->bindParam(":id", $_GET['delId'], PDO::PARAM_INT);
         $count->execute();
